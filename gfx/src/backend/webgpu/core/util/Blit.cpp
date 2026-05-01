504b6cc536250094ec760ad205ec62efea425955
helcl42 <lubos.helcl@gmail.com>
2026-05-01 03:36:58 +0200

    Extend blit

--- gfx/src/backend/webgpu/core/util/Blit.cpp
+++ gfx/src/backend/webgpu/core/util/Blit.cpp
@@ -8,6 +8,12 @@ namespace gfx::backend::webgpu::core {
 Blit::Blit(WGPUDevice device)
     : m_device(device)
 {
+    initBlit2D();
+    initBlit3D();
+}
+
+void Blit::initBlit2D()
+{
     // Create shader module for 2D textures with source region support
     const char* shader2DCode = R"(
             struct SourceRegion {
@@ -78,49 +84,201 @@ Blit::Blit(WGPUDevice device)
     m_pipelineLayout = wgpuDeviceCreatePipelineLayout(m_device, &pipelineLayoutDesc);
 }
 
+void Blit::initBlit3D()
+{
+    // Create compute shader for 3D texture mipmap downsampling
+    const char* shader3DCode = R"(
+            @group(0) @binding(0) var srcTexture: texture_3d<f32>;
+            @group(0) @binding(1) var dstTexture: texture_storage_3d<rgba8unorm, write>;
+
+            @compute @workgroup_size(4, 4, 4)
+            fn main(@builtin(global_invocation_id) id: vec3u) {
+                let dstSize = textureDimensions(dstTexture);
+                if (id.x >= dstSize.x || id.y >= dstSize.y || id.z >= dstSize.z) {
+                    return;
+                }
+
+                // Sample 8 texels from the source mip level (2x2x2 box filter)
+                let srcCoord = id * 2u;
+                let s000 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(0u, 0u, 0u)), 0);
+                let s100 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(1u, 0u, 0u)), 0);
+                let s010 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(0u, 1u, 0u)), 0);
+                let s110 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(1u, 1u, 0u)), 0);
+                let s001 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(0u, 0u, 1u)), 0);
+                let s101 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(1u, 0u, 1u)), 0);
+                let s011 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(0u, 1u, 1u)), 0);
+                let s111 = textureLoad(srcTexture, vec3i(srcCoord + vec3u(1u, 1u, 1u)), 0);
+
+                let result = (s000 + s100 + s010 + s110 + s001 + s101 + s011 + s111) * 0.125;
+                textureStore(dstTexture, vec3i(id), result);
+            }
+        )";
+
+    WGPUShaderSourceWGSL wgslSource = WGPU_SHADER_SOURCE_WGSL_INIT;
+    wgslSource.chain.sType = WGPUSType_ShaderSourceWGSL;
+    wgslSource.code = toStringView(shader3DCode);
+
+    WGPUShaderModuleDescriptor shaderDesc = WGPU_SHADER_MODULE_DESCRIPTOR_INIT;
+    shaderDesc.nextInChain = &wgslSource.chain;
+    m_shaderModule3D = wgpuDeviceCreateShaderModule(m_device, &shaderDesc);
+
+    // Bind group layout: binding 0 = texture_3d, binding 1 = storage texture 3D write
+    WGPUBindGroupLayoutEntry bgLayoutEntries3D[2] = { WGPU_BIND_GROUP_LAYOUT_ENTRY_INIT, WGPU_BIND_GROUP_LAYOUT_ENTRY_INIT };
+    bgLayoutEntries3D[0].binding = 0;
+    bgLayoutEntries3D[0].visibility = WGPUShaderStage_Compute;
+    bgLayoutEntries3D[0].texture.sampleType = WGPUTextureSampleType_Float;
+    bgLayoutEntries3D[0].texture.viewDimension = WGPUTextureViewDimension_3D;
+
+    bgLayoutEntries3D[1].binding = 1;
+    bgLayoutEntries3D[1].visibility = WGPUShaderStage_Compute;
+    bgLayoutEntries3D[1].storageTexture.access = WGPUStorageTextureAccess_WriteOnly;
+    bgLayoutEntries3D[1].storageTexture.format = WGPUTextureFormat_RGBA8Unorm;
+    bgLayoutEntries3D[1].storageTexture.viewDimension = WGPUTextureViewDimension_3D;
+
+    WGPUBindGroupLayoutDescriptor bgLayoutDesc3D = WGPU_BIND_GROUP_LAYOUT_DESCRIPTOR_INIT;
+    bgLayoutDesc3D.entryCount = 2;
+    bgLayoutDesc3D.entries = bgLayoutEntries3D;
+    m_bindGroupLayout3D = wgpuDeviceCreateBindGroupLayout(m_device, &bgLayoutDesc3D);
+
+    WGPUPipelineLayoutDescriptor pipelineLayoutDesc3D = WGPU_PIPELINE_LAYOUT_DESCRIPTOR_INIT;
+    pipelineLayoutDesc3D.bindGroupLayoutCount = 1;
+    pipelineLayoutDesc3D.bindGroupLayouts = &m_bindGroupLayout3D;
+    m_pipelineLayout3D = wgpuDeviceCreatePipelineLayout(m_device, &pipelineLayoutDesc3D);
+
+    WGPUComputePipelineDescriptor computeDesc = WGPU_COMPUTE_PIPELINE_DESCRIPTOR_INIT;
+    computeDesc.layout = m_pipelineLayout3D;
+    computeDesc.compute.module = m_shaderModule3D;
+    computeDesc.compute.entryPoint = toStringView("main");
+    m_pipeline3D = wgpuDeviceCreateComputePipeline(m_device, &computeDesc);
+}
+
 Blit::~Blit()
 {
-    // Release cached pipelines
+    destroyBlit2D();
+    destroyBlit3D();
+}
+
+void Blit::destroyBlit2D()
+{
     for (auto& pair : m_pipelines) {
         wgpuRenderPipelineRelease(pair.second);
     }
-    // Release cached samplers
+    m_pipelines.clear();
     for (auto& pair : m_samplers) {
         wgpuSamplerRelease(pair.second);
     }
+    m_samplers.clear();
     if (m_pipelineLayout) {
         wgpuPipelineLayoutRelease(m_pipelineLayout);
+        m_pipelineLayout = nullptr;
     }
     if (m_bindGroupLayout) {
         wgpuBindGroupLayoutRelease(m_bindGroupLayout);
+        m_bindGroupLayout = nullptr;
     }
     if (m_shaderModule) {
         wgpuShaderModuleRelease(m_shaderModule);
+        m_shaderModule = nullptr;
+    }
+}
+
+void Blit::destroyBlit3D()
+{
+    if (m_pipeline3D) {
+        wgpuComputePipelineRelease(m_pipeline3D);
+        m_pipeline3D = nullptr;
+    }
+    if (m_pipelineLayout3D) {
+        wgpuPipelineLayoutRelease(m_pipelineLayout3D);
+        m_pipelineLayout3D = nullptr;
+    }
+    if (m_bindGroupLayout3D) {
+        wgpuBindGroupLayoutRelease(m_bindGroupLayout3D);
+        m_bindGroupLayout3D = nullptr;
+    }
+    if (m_shaderModule3D) {
+        wgpuShaderModuleRelease(m_shaderModule3D);
+        m_shaderModule3D = nullptr;
     }
 }
 
 void Blit::execute(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, const WGPUOrigin3D& srcOrigin, const WGPUExtent3D& srcExtent, uint32_t srcMipLevel, WGPUTexture dstTexture, const WGPUOrigin3D& dstOrigin, const WGPUExtent3D& dstExtent, uint32_t dstMipLevel, WGPUFilterMode filterMode)
 {
+    WGPUTextureDimension srcDimension = wgpuTextureGetDimension(srcTexture);
+
+    if (srcDimension == WGPUTextureDimension_3D) {
+        execute3D(commandEncoder, srcTexture, srcMipLevel, dstTexture, dstMipLevel);
+    } else if (srcDimension == WGPUTextureDimension_2D) {
+        execute2D(commandEncoder, srcTexture, srcOrigin, srcExtent, srcMipLevel, dstTexture, dstOrigin, dstExtent, dstMipLevel, filterMode);
+    } else {
+        gfx::common::Logger::instance().logWarning("[WebGPU Blit] 1D textures are not supported for blitting");
+    }
+}
+
+void Blit::execute3D(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, uint32_t srcMipLevel, WGPUTexture dstTexture, uint32_t dstMipLevel)
+{
+    // Create 3D texture views for src and dst mip levels
+    WGPUTextureViewDescriptor srcViewDesc = WGPU_TEXTURE_VIEW_DESCRIPTOR_INIT;
+    srcViewDesc.format = wgpuTextureGetFormat(srcTexture);
+    srcViewDesc.dimension = WGPUTextureViewDimension_3D;
+    srcViewDesc.baseMipLevel = srcMipLevel;
+    srcViewDesc.mipLevelCount = 1;
+    srcViewDesc.baseArrayLayer = 0;
+    srcViewDesc.arrayLayerCount = 1;
+    WGPUTextureView srcView = wgpuTextureCreateView(srcTexture, &srcViewDesc);
+
+    WGPUTextureViewDescriptor dstViewDesc = WGPU_TEXTURE_VIEW_DESCRIPTOR_INIT;
+    dstViewDesc.format = wgpuTextureGetFormat(dstTexture);
+    dstViewDesc.dimension = WGPUTextureViewDimension_3D;
+    dstViewDesc.baseMipLevel = dstMipLevel;
+    dstViewDesc.mipLevelCount = 1;
+    dstViewDesc.baseArrayLayer = 0;
+    dstViewDesc.arrayLayerCount = 1;
+    WGPUTextureView dstView = wgpuTextureCreateView(dstTexture, &dstViewDesc);
+
+    // Create bind group
+    WGPUBindGroupEntry bgEntries[2] = { WGPU_BIND_GROUP_ENTRY_INIT, WGPU_BIND_GROUP_ENTRY_INIT };
+    bgEntries[0].binding = 0;
+    bgEntries[0].textureView = srcView;
+    bgEntries[1].binding = 1;
+    bgEntries[1].textureView = dstView;
+
+    WGPUBindGroupDescriptor bgDesc = WGPU_BIND_GROUP_DESCRIPTOR_INIT;
+    bgDesc.layout = m_bindGroupLayout3D;
+    bgDesc.entryCount = 2;
+    bgDesc.entries = bgEntries;
+    WGPUBindGroup bindGroup = wgpuDeviceCreateBindGroup(m_device, &bgDesc);
+
+    // Calculate destination mip dimensions
+    uint32_t dstWidth = std::max(1u, wgpuTextureGetWidth(dstTexture) >> dstMipLevel);
+    uint32_t dstHeight = std::max(1u, wgpuTextureGetHeight(dstTexture) >> dstMipLevel);
+    uint32_t dstDepth = std::max(1u, wgpuTextureGetDepthOrArrayLayers(dstTexture) >> dstMipLevel);
+
+    // Dispatch compute shader
+    WGPUComputePassDescriptor passDesc = WGPU_COMPUTE_PASS_DESCRIPTOR_INIT;
+    WGPUComputePassEncoder computePass = wgpuCommandEncoderBeginComputePass(commandEncoder, &passDesc);
+    wgpuComputePassEncoderSetPipeline(computePass, m_pipeline3D);
+    wgpuComputePassEncoderSetBindGroup(computePass, 0, bindGroup, 0, nullptr);
+    wgpuComputePassEncoderDispatchWorkgroups(computePass,
+        (dstWidth + 3) / 4, (dstHeight + 3) / 4, (dstDepth + 3) / 4);
+    wgpuComputePassEncoderEnd(computePass);
+
+    // Cleanup
+    wgpuComputePassEncoderRelease(computePass);
+    wgpuBindGroupRelease(bindGroup);
+    wgpuTextureViewRelease(dstView);
+    wgpuTextureViewRelease(srcView);
+}
+
+void Blit::execute2D(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, const WGPUOrigin3D& srcOrigin, const WGPUExtent3D& srcExtent, uint32_t srcMipLevel, WGPUTexture dstTexture, const WGPUOrigin3D& dstOrigin, const WGPUExtent3D& dstExtent, uint32_t dstMipLevel, WGPUFilterMode filterMode)
+{
     // Get or create sampler with requested filter mode
     WGPUSampler sampler = getOrCreateSampler(filterMode);
-
-    // Query texture dimension - use 2D for now, can be extended for arrays/3D later
-    WGPUTextureDimension srcDimension = wgpuTextureGetDimension(srcTexture);
-    WGPUTextureViewDimension viewDimension = WGPUTextureViewDimension_2D;
-
-    // Map texture dimension to view dimension
-    // For now we only support 2D, but this can be extended
-    if (srcDimension == WGPUTextureDimension_2D) {
-        viewDimension = WGPUTextureViewDimension_2D;
-    } else {
-        // 1D and 3D textures not yet supported by the shader
-        gfx::common::Logger::instance().logWarning("[WebGPU Blit] Only 2D textures are currently supported");
-    }
 
     // Create texture view for source
     WGPUTextureViewDescriptor srcViewDesc = WGPU_TEXTURE_VIEW_DESCRIPTOR_INIT;
     srcViewDesc.format = wgpuTextureGetFormat(srcTexture);
-    srcViewDesc.dimension = viewDimension;
+    srcViewDesc.dimension = WGPUTextureViewDimension_2D;
     srcViewDesc.baseMipLevel = srcMipLevel;
     srcViewDesc.mipLevelCount = 1;
     srcViewDesc.baseArrayLayer = 0;
--- gfx/src/backend/webgpu/core/util/Blit.h
+++ gfx/src/backend/webgpu/core/util/Blit.h
@@ -19,15 +19,31 @@ public:
     void execute(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, const WGPUOrigin3D& srcOrigin, const WGPUExtent3D& srcExtent, uint32_t srcMipLevel, WGPUTexture dstTexture, const WGPUOrigin3D& dstOrigin, const WGPUExtent3D& dstExtent, uint32_t dstMipLevel, WGPUFilterMode filterMode);
 
 private:
+    void execute2D(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, const WGPUOrigin3D& srcOrigin, const WGPUExtent3D& srcExtent, uint32_t srcMipLevel, WGPUTexture dstTexture, const WGPUOrigin3D& dstOrigin, const WGPUExtent3D& dstExtent, uint32_t dstMipLevel, WGPUFilterMode filterMode);
+    void execute3D(WGPUCommandEncoder commandEncoder, WGPUTexture srcTexture, uint32_t srcMipLevel, WGPUTexture dstTexture, uint32_t dstMipLevel);
+
     WGPURenderPipeline getOrCreatePipeline(WGPUTextureFormat format);
     WGPUSampler getOrCreateSampler(WGPUFilterMode filterMode);
 
+    void initBlit2D();
+    void destroyBlit2D();
+    void initBlit3D();
+    void destroyBlit3D();
+
     WGPUDevice m_device = nullptr;
+
+    // 2D blit resources
     WGPUShaderModule m_shaderModule = nullptr;
     WGPUBindGroupLayout m_bindGroupLayout = nullptr;
     WGPUPipelineLayout m_pipelineLayout = nullptr;
     std::unordered_map<WGPUTextureFormat, WGPURenderPipeline> m_pipelines;
     std::unordered_map<WGPUFilterMode, WGPUSampler> m_samplers;
+
+    // 3D blit (compute) resources
+    WGPUShaderModule m_shaderModule3D = nullptr;
+    WGPUBindGroupLayout m_bindGroupLayout3D = nullptr;
+    WGPUPipelineLayout m_pipelineLayout3D = nullptr;
+    WGPUComputePipeline m_pipeline3D = nullptr;
 };
 
 } // namespace gfx::backend::webgpu::core
